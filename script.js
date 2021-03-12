66a7bfc08f7c07dc8df698af3c756b95d93760ef
mariangz <marianosgimenez@gmail.com>
2021-03-12 10:57:47 +0100

    feat: add player and computer selection

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 "use strict"
 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock");
+}
+
+//player selection
 const rockBtn = document.querySelector("#rock");
 rockBtn.addEventListener("click", () => {
-    document.body.style.backgroundColor = "yellow";
+    playRound("rock", computerPlay());
+
 })
 
 const paperBtn = document.querySelector("#paper");
 paperBtn.addEventListener("click", () => {
-    document.body.style.backgroundColor = "red";
+    playRound("paper", computerPlay());
 })
 
 const scissorsBtn = document.querySelector("#scissors");
 scissorsBtn.addEventListener("click", () => {
-    document.body.style.backgroundColor = "green";
+    playRound("scissors", computerPlay());
 })
 
-const computerBtn = document.querySelector("#computer");
-computerBtn.addEventListener("click", computerPlay)
-
-function playRound(playerSelection, computerSelection) {
-    // your code here!
+//computer selection
+function computerPlay(computerSelection) {
+    computerSelection = Math.floor(Math.random() * 3);
+    switch (computerSelection) {
+        case 0:
+            //console.log("rock");
+            return "rock";
+            break;
+        case 1:
+            //console.log("paper")
+            return "paper";
+            break;
+        case 2:
+            //console.log("scissors")
+            return "scissors";
+            break;
+    }
 }
-
-function playerSelection() {
-
-}
-
-
-function computerPlay() {
-    let randomNum = Math.floor(Math.random() * 3);
-    alert(randomNum);
-    // switch (randomNum) {
-    //     case 0:
-    //         return "rock";
-    //         break;
-    //     case 1:
-    //         return "paper";
-    //         break;
-    //     case 2:
-    //         return "scissors";
-    //         break;
-    // }
-}
-
